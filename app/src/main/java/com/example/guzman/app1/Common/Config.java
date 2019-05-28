f7644576d660bbbe938c0553c1bb8eaa3696919f
joserumpe <jrumpe@gmail.com>
2019-05-28 23:01:07 +0300

    remove token. security issue.

--- app/src/main/java/com/example/guzman/app1/Common/Config.java
+++ app/src/main/java/com/example/guzman/app1/Common/Config.java
@@ -2,5 +2,5 @@ package com.example.guzman.app1.Common;
 
 public class Config {
 
-    public static final String PAY_PAL_CLIENT ="AUKB9NqKP9MybHG8NrIHXpuEO7UdzbN05M7c5gVqV5Kayz-g1WsGyftWOrMA07SFqH-qtRPYQaa4JMyz";
+    
 }
